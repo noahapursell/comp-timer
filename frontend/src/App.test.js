0b33010827f79641fdc55aa2cddc2670da5d2ed2
Noah <73550955+noahapursell@users.noreply.github.com>
2025-06-03 20:05:26 -0500

    Update App test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Timers Dashboard heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Timers Dashboard/i);
+  expect(headingElement).toBeInTheDocument();
 });
